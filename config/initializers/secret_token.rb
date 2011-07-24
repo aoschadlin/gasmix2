f8c14fe6a4756b2cdb65301bd8514880754c0702
Andre Oschadlin <andreoschadlin@gmail.com>
2011-07-24 09:42:04 -0700

    Initial creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gasmix2::Application.config.secret_token = '3d1df66ac788f7b5e65144cb8567c86af8c86b6bbd3238e142b4667ac9d7f706c6ade1d6c5137bd92b3e1d04e265e3dee55c34f887a613220eec00777838abd4'
